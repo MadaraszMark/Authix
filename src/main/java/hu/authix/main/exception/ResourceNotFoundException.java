6e2aba7941d4814f1a968a48a97682cdbd6c4727
MadaraszMark <madaraszmark461@gmail.com>
2025-11-20 10:40:21 +0100

    Add: Global Exception

---
+++ src/main/java/hu/authix/main/exception/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package hu.authix.main.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    
+    public ResourceNotFoundException(String resource, Long id) {
+        super(resource + " nem található ezzel az azonosítóval: " + id);
+    }
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
